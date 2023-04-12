4fd23299ed025e64e0a01833a4df6765d7194e01
JustRed23 <manuel.bormans@gmail.com>
2023-04-12 19:37:06 +0200

    Remove old classes

--- src/main/java/dev/JustRed23/redbit/ex/SceneInitializationException.java
+++
@@ -1,8 +0,0 @@
-package dev.JustRed23.redbit.ex;
-
-public class SceneInitializationException extends Exception {
-
-    public SceneInitializationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
