c67f645f6d7a5048a5e4680688b1583f9bb7e454
JustRed23 <manuel.bormans@gmail.com>
2023-04-17 08:29:50 +0200

    Disable depth test

--- src/main/java/dev/JustRed23/redbit/engine/window/Window.java
+++ src/main/java/dev/JustRed23/redbit/engine/window/Window.java
@@ -172,8 +172,7 @@ public class Window {
         if (!visible || !focused)
             return;
 
-        glEnable(GL_DEPTH_TEST);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         if (currentView != null) {
